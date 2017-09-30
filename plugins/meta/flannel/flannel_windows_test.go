979edd1df65cab0d688ff3e394438852c2f299c0
Rakesh Kelkar <rakelkar@gmail.com>
2017-09-29 20:18:47 -0700

    add tests for windows fns

--- plugins/meta/flannel/flannel_windows_test.go
+++ plugins/meta/flannel/flannel_windows_test.go
@@ -14,13 +14,75 @@
 package main
 
 import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-
 	"testing"
+	"net"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestFlannel(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Flannel Suite")
+func TestFlannelSetsWindowsOutboutNat(t *testing.T) {
+	// given a subnet config that requests ipmasq
+	_, nw, _ := net.ParseCIDR("192.168.0.0/16")
+	_, sn, _ := net.ParseCIDR("192.168.10.0/24")
+	ipmasq := true
+	fenv := &subnetEnv{
+		nw: nw,
+		sn: sn,
+		ipmasq:	&ipmasq,
+	}
+
+	// apply it
+	delegate := make(map[string]interface{})
+	err := updateOutboundNat(&delegate, fenv)
+
+	// verify it got applied
+	assert.Nil(t, err)
+	assert.True(t, hasKey(delegate, "AdditionalArgs"))
+
+	addlArgs := (delegate["AdditionalArgs"]).([]interface{})
+	assert.Equal(t, 1, len(addlArgs))
+
+	policy := addlArgs[0].(map[string]interface{})
+	assert.True(t, hasKey(policy, "Name"))
+	assert.True(t, hasKey(policy, "Value"))
+	assert.Equal(t, "EndpointPolicy", policy["Name"])
+
+	value := policy["Value"].(map[string]interface{})
+	assert.True(t, hasKey(value, "Type"))
+	assert.True(t, hasKey(value, "ExceptionList"))
+	assert.Equal(t, "OutBoundNAT", value["Type"])
+
+	exceptionList := value["ExceptionList"].([]interface{})
+	assert.Equal(t, 1, len(exceptionList))
+	assert.Equal(t, nw.String(), exceptionList[0].(string))
+}
+
+func TestFlannelSkipsIfOutboutNatAlreadySet(t *testing.T) {
+
+	// given a subnet config that requests ipmasq
+	_, nw, _ := net.ParseCIDR("192.168.0.0/16")
+	_, sn, _ := net.ParseCIDR("192.168.10.0/24")
+	ipmasq := true
+	fenv := &subnetEnv{
+		nw: nw,
+		sn: sn,
+		ipmasq:	&ipmasq,
+	}
+
+	// first set it
+	delegate := make(map[string]interface{})
+	err := updateOutboundNat(&delegate, fenv)
+	assert.Nil(t, err)
+
+	// then attempt to update it
+	_, nw2, _ := net.ParseCIDR("10.244.0.0/16")
+	fenv.nw = nw2
+	err = updateOutboundNat(&delegate, fenv)
+
+	// but it stays the same!
+	addlArgs := (delegate["AdditionalArgs"]).([]interface{})
+	policy := addlArgs[0].(map[string]interface{})
+	value := policy["Value"].(map[string]interface{})
+	exceptionList := value["ExceptionList"].([]interface{})
+	assert.Equal(t, nw.String(), exceptionList[0].(string))
+	assert.NotEqual(t, nw2.String(), exceptionList[0].(string))
 }
